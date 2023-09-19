9665e30b34c6cc5cc78f6ed6d33cc52d9b4e9fe8
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-09-19 17:10:50 +0700

    Responsive Navbar Design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import ThemeToggle from "../themeToggle/ThemeToggle";
 import styles from "./navbar.module.css";
 const Navbar = () => {
-  return <div className={styles.container}>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="" width={24} height={24} />
+        <Image src="/instagram.png" alt="" width={24} height={24} />
+        <Image src="/tiktok.png" alt="" width={24} height={24} />
+        <Image src="/youtube.png" alt="" width={24} height={24} />
+      </div>
+      <div className={styles.logo}>KhoaBlog</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/" className={styles.link}>
+          Homepage
+        </Link>
+        <Link href="/" className={styles.link}>
+          Contact
+        </Link>
+        <Link href="/" className={styles.link}>
+          About
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
