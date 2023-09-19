ab2b909f336e27416fc10e9b5e45a60e3e514e77
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-09-19 12:00:42 +0700

    fix structure folder

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,74 +4,7 @@ import styles from "./homepage.module.css";
 export default function Home() {
   return (
     <div>
-      <Link href="/">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus
-        nemo commodi minus ipsa fugiat cum maxime magni a hic vel nesciunt
-        officia perferendis, facilis recusandae quam, distinctio facere ipsum!
-        Recusandae. Officiis magnam, quos quia deleniti sit voluptatum porro ab
-        quae voluptatibus assumenda vitae consequatur, veritatis perferendis.
-        Dolorem facere cum maiores at rem officia? Voluptatem consectetur in
-        neque minus eius saepe. Veritatis quod consequuntur repellat incidunt
-        sed voluptatem omnis molestias in nobis dignissimos, pariatur cumque
-        temporibus saepe magni accusamus id corporis quos. Earum voluptatum
-        sapiente quaerat at omnis a recusandae animi. Repudiandae voluptatum
-        dolores magni ullam quasi at, ratione beatae pariatur voluptas dicta,
-        quae expedita delectus, eum commodi vitae fugit reiciendis cumque nemo
-        officia necessitatibus repellendus? Non facilis dolor cumque eius! Esse,
-        harum. Eligendi, non asperiores. Sunt tempore quod assumenda dignissimos
-        velit minima reprehenderit in, eligendi dolorum ducimus ad ex quibusdam
-        architecto molestias cum enim, earum dicta culpa repudiandae modi saepe!
-        Quod modi omnis nemo ut. Ipsa, sunt ipsum amet veritatis exercitationem
-        accusamus harum et consequatur! Hic minus, veniam quae vero, illo
-        perspiciatis aliquid nesciunt saepe harum, praesentium quas iure maxime.
-        Nemo odio perferendis aliquid? Eligendi vero eum magni, soluta
-        reiciendis dolore distinctio totam officiis cumque quos! Totam illum
-        fugiat, labore ratione nemo, suscipit soluta repellendus veniam, ad non
-        laudantium perspiciatis. Culpa esse in molestiae itaque cumque rerum,
-        enim architecto perspiciatis assumenda earum suscipit quis labore
-        facilis accusamus eos! Nihil sint pariatur iusto molestias consequatur,
-        harum culpa ullam incidunt esse quasi? Atque alias eveniet perspiciatis
-        optio nemo animi dolor nobis officia excepturi dolore hic facilis
-        aliquid est id possimus incidunt ea libero maiores eos placeat aut,
-        porro asperiores rerum? Totam, porro? Magni quasi quae deleniti vitae,
-        similique praesentium repellat et est. Magnam vel nemo accusamus harum,
-        amet explicabo iste assumenda cupiditate aliquam eveniet fuga neque
-        laboriosam, ex tenetur a deleniti consequuntur! Deleniti voluptatum
-        velit consequatur repudiandae ipsum molestias rem repellendus tenetur
-        modi perferendis animi iure ex ad explicabo at libero, aut impedit sunt
-        dolores saepe a. Sit quaerat neque rem natus. Amet ipsum cumque sapiente
-        dolorum quibusdam doloribus suscipit possimus totam eligendi at
-        voluptatum sunt cupiditate officiis optio accusamus odit, reprehenderit
-        non itaque rerum magnam repellat soluta eos dignissimos quidem? Harum?
-        Error libero quas, veritatis assumenda quidem mollitia voluptas saepe
-        odio quis, optio autem? Tenetur dignissimos amet non sapiente eum est
-        quos quod quisquam, provident, debitis, optio ea adipisci fugiat
-        aperiam! Dolore consequatur debitis consequuntur magnam velit sequi ea
-        repudiandae beatae eum aspernatur. Numquam vel quam enim reprehenderit
-        laboriosam laudantium nostrum aliquam quos exercitationem cumque. Quia
-        iusto aliquam impedit molestias facilis. Quasi assumenda cumque,
-        praesentium iusto veritatis facilis veniam perspiciatis porro doloribus
-        reiciendis? Rerum eaque magnam at, accusamus quis voluptatem, aut
-        blanditiis alias quos, officia illum cumque necessitatibus labore
-        aliquid. Qui. Animi dicta labore atque laudantium ipsa nulla alias,
-        autem totam cupiditate blanditiis iste laborum assumenda aspernatur
-        reprehenderit ab? Accusantium amet voluptatum provident quam nesciunt
-        perspiciatis. Placeat quibusdam eius laborum soluta! Voluptas iusto quia
-        accusamus eaque alias suscipit quibusdam, id sed maiores. Obcaecati
-        quibusdam similique unde earum. Incidunt perferendis, deleniti commodi
-        voluptate fugit neque pariatur nobis, blanditiis excepturi eius illo
-        exercitationem. Soluta, quod. Consectetur tempore quaerat quidem!
-        Repellat expedita ipsum eius numquam necessitatibus debitis ea iste
-        quibusdam placeat repudiandae nemo porro incidunt ullam deserunt
-        perferendis, reiciendis dolor corporis odio nulla sunt. Iure expedita
-        nulla minus accusantium minima repellendus, veritatis voluptas. Magnam
-        molestiae officia ullam! Deserunt facere recusandae impedit id voluptate
-        asperiores sequi sint. Sequi doloremque sint eveniet aliquam nam
-        architecto quisquam! Eligendi maxime dolorum nostrum quod neque atque
-        aliquid, illo, sint maiores aspernatur vitae itaque accusamus esse
-        reprehenderit, saepe rerum fugit iure ullam excepturi ad commodi
-        tempora? Ipsum quisquam voluptatem atque!
-      </Link>
+      <Link href="/">Hello</Link>
     </div>
   );
 }
